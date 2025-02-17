dd7e0f183c115e8eda7815256b14cafe77ff51ad
ghplayer2000 <ghplayer2000@gmail.com>
2025-02-16 22:06:03 -0300

    Update 10 files: app_config.json, build.gradle, libraries.json, AndroidManifest.xml, MainActivity.java, activity_main.xml, styles.xml, darktheme.css, index.html, noteview.js

--- android/app/src/main/java/com/gh/metro/notes/MainActivity.java
+++ android/app/src/main/java/com/gh/metro/notes/MainActivity.java
@@ -1,64 +1,130 @@
 package com.gh.metro.notes;
 
-import android.app.Activity;
-import android.net.Uri;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowInsetsControllerCompat;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
-	//declaration
-	private WebView frame;
+	private WebView webView;
+	private boolean isAppJustStarted = true; // Variável de controle
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		//this is the reference for the webview in layout/activity_main.xml.
-		frame = (WebView) findViewById(R.id.webview);
+		webView = findViewById(R.id.webview);
+		WebSettings webSettings = webView.getSettings();
 
-		WebSettings webSettings = frame.getSettings();
-
-		//need javascript in the Webview? don't comment this out.
 		webSettings.setJavaScriptEnabled(true);
 		webSettings.setDomStorageEnabled(true);
-		frame.setWebChromeClient(new WebChromeClient());
+		webView.setWebChromeClient(new WebChromeClient());
 
-		//this setting here is to prevent the Webview from opening links in a new window.
-		frame.setWebViewClient(new WebViewClient() {
+		webView.setWebViewClient(new WebViewClient() {
+			@Override
 			public boolean shouldOverrideUrlLoading(WebView view, String url) {
 				if (url.equals("https://github.com/Ghplayer2000/metro-notes/")) {
 					Intent i = new Intent(Intent.ACTION_VIEW);
 					i.setData(Uri.parse(url));
 					startActivity(i);
-
 					return true;
 				}
-
 				return false;
+			}
+
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				runOnUiThread(() -> {
+					applyTheme();
+					isAppJustStarted = false; // Define como falso após o primeiro carregamento
+				});
 			}
 		});
 
-		//one is for loading a URL (external-only for production lol)...
-		//the other is for loading an in-app HTML file (located in X).
+		webView.addJavascriptInterface(new WebAppInterface(this), "InterfaceAndroid");
 
-		//frame.loadUrl("http://10.0.2.2/");
-		frame.loadUrl("file:///android_asset/web_content/index.html");
+		webView.loadUrl("file:///android_asset/web_content/index.html");
+
 	}
 
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-
-		if ((keyCode == KeyEvent.KEYCODE_BACK) && frame.canGoBack()) {
-			frame.goBack();
+		if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+			webView.goBack();
 			return true;
 		}
 		return super.onKeyDown(keyCode, event);
 	}
+
+	public class WebAppInterface {
+		Context mContext;
+
+		WebAppInterface(Context c) {
+			mContext = c;
+		}
+
+		@JavascriptInterface
+		public void changeColor(String theme) {
+			SharedPreferences prefs = mContext.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+			SharedPreferences.Editor editor = prefs.edit();
+			editor.putString("theme", theme);
+			editor.apply();
+
+			// Verifica se o aplicativo acabou de iniciar
+			if (!isAppJustStarted) { // Só mostra o diálogo se não for a inicialização
+				Intent intent = new Intent(mContext, MainActivity.class);
+				intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+				mContext.startActivity(intent);
+			} else {
+				// Se o app acabou de iniciar, apenas aplica o tema, sem reiniciar
+				applyTheme();
+			}
+		}
+	}
+
+	private void applyTheme() {
+		SharedPreferences prefs = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+		String theme = prefs.getString("theme", "light");
+		Window window = getWindow();
+		View decorView = window.getDecorView();
+
+		WindowInsetsControllerCompat wic = new WindowInsetsControllerCompat(window, decorView);
+
+		if (theme.equals("light")) {
+			getWindow().setStatusBarColor(Color.parseColor("#FFFFFF"));
+			getWindow().setNavigationBarColor(Color.parseColor("#DDDDDD"));
+			wic.setAppearanceLightStatusBars(true);
+			wic.setAppearanceLightNavigationBars(true);
+		} else if (theme.equals("dark")) {
+			getWindow().setStatusBarColor(Color.parseColor("#000000"));
+			getWindow().setNavigationBarColor(Color.parseColor("#1F1F1F"));
+			wic.setAppearanceLightStatusBars(false);
+			wic.setAppearanceLightNavigationBars(false);
+		} else {
+			getWindow().setStatusBarColor(Color.parseColor("#FFFFFF"));
+			getWindow().setNavigationBarColor(Color.parseColor("#DDDDDD"));
+			wic.setAppearanceLightStatusBars(true);
+			wic.setAppearanceLightNavigationBars(true);
+
+		}
+
+	}
+
 }
--- js/noteview.js
+++ js/noteview.js
@@ -7,6 +7,8 @@ const aboutButton = document.getElementById('aboutButton');
 const notesContainer = document.getElementById('notesContainer');
 const pageTitle = document.getElementById('pageTitle');
 const appName = document.getElementById('appName');
+const appbar = document.getElementById('appbar');
+const darkbtn = document.getElementById('darkbtn');
 
 let selectedNote;
 let timer;
@@ -14,122 +16,170 @@ let timer;
 showNotes();
 
 window.setTimeout(() => {
-    notesContainer.classList.remove('moveFromLeft');
-    pageTitle.classList.remove('moveFromLeft');
-    appName.classList.remove('moveFromLeft');
+	notesContainer.classList.remove('moveFromLeft');
+	pageTitle.classList.remove('moveFromLeft');
+	appName.classList.remove('moveFromLeft');
 }, 600);
 
 function showNotes() {
-    let notesHTML = '';
-    let notes = localStorage.getItem('notes');
-    if (notes === null) {
-        return;
-    } else {
-        notes = JSON.parse(notes);
-    }
-    for (let i = 0; i < notes.length; i++) {
-        notesHTML += `<div class="note" data-index="${i}">
-                    <img src="svg/file.svg" alt="Note Icon"/>
-                    <span><p style="font-size: 38px;">${notes[i].title === "" ? 'Note' : notes[i].title}</p></span>
-                </div>
-        `;
-    }
-    notesContainer.innerHTML = notesHTML;
+	let notesHTML = '';
+	let notes = localStorage.getItem('notes');
+	if (notes === null) {
+		return;
+	} else {
+		notes = JSON.parse(notes);
+	}
+	for (let i = 0; i < notes.length; i++) {
+		notesHTML += `<div class="note" data-index="${i}">
+		<img id="img" src="svg/file.svg" alt="Note Icon"/>
+		<span><p style="font-size: 38px;">${notes[i].title === "" ? 'Note': notes[i].title}</p></span>
+		</div>
+		`;
+	}
+	notesContainer.innerHTML = notesHTML;
 
-    const noteElements = document.querySelectorAll('.note');
-    noteElements.forEach(noteElement => {
-        noteElement.addEventListener('click', openEditor);
+	const noteElements = document.querySelectorAll('.note');
+	noteElements.forEach(noteElement => {
+		noteElement.addEventListener('click', openEditor);
 
-        noteElement.addEventListener('touchstart', (event) => {
-            event.preventDefault();
-            timer = setTimeout(() => {
-                selectedNote = event.target.closest('.note');
-                selectedNote.classList.add('selected');
-                showPopup(selectedNote);
-            }, 500);
-        });
+		noteElement.addEventListener('touchstart', (event) => {
+			event.preventDefault();
+			timer = setTimeout(() => {
+				selectedNote = event.target.closest('.note');
+				selectedNote.classList.add('selected');
+				showPopup(selectedNote);
+			}, 500);
+		});
 
-        noteElement.addEventListener('touchend', (event) => {
-            clearTimeout(timer);
-            if (!popup.classList.contains('active')) {
-                openEditor(event);
-            }
-        });
-    });
+		noteElement.addEventListener('touchend', (event) => {
+			clearTimeout(timer);
+			if (!popup.classList.contains('active')) {
+				openEditor(event);
+			}
+		});
+	});
 }
 
 deleteLink.addEventListener('click', () => {
-    if (selectedNote) {
-        deleteNote(selectedNote.dataset.index);
-        hidePopup();
-    }
+	if (selectedNote) {
+		deleteNote(selectedNote.dataset.index);
+		hidePopup();
+	}
 });
 
 overlay.addEventListener('click', hidePopup);
 
 function showPopup(note) {
-    const rect = note.getBoundingClientRect();
-    popup.style.top = rect.bottom + 'px';
-    popup.style.display = 'block';
-    popup.classList.add('active');
-    overlay.classList.add('active');
+	const rect = note.getBoundingClientRect();
+	popup.style.top = rect.bottom + 'px';
+	popup.style.display = 'block';
+	popup.classList.add('active');
+	overlay.classList.add('active');
 
-    const dimmedElements = document.querySelectorAll('.note:not(.selected), #top');
-    dimmedElements.forEach(element => {
-        element.classList.remove('dimmedRestore');
-        element.classList.add('dimmed');
-    });
+	const dimmedElements = document.querySelectorAll('.note:not(.selected), #top');
+	dimmedElements.forEach(element => {
+		element.classList.remove('dimmedRestore');
+		element.classList.add('dimmed');
+	});
 
-    topHeader.classList.add('dimmed');
-    newNoteButton.classList.add('dimmed');
-    aboutButton.classList.add('dimmed');
+	topHeader.classList.add('dimmed');
+	newNoteButton.classList.add('dimmed');
+	aboutButton.classList.add('dimmed');
 
-    overlay.style.pointerEvents = 'auto';
+	overlay.style.pointerEvents = 'auto';
 }
 
 function hidePopup() {
-    popup.style.display = 'none';
-    popup.classList.remove('active');
-    overlay.classList.remove('active');
-    overlay.style.pointerEvents = 'none';
+	popup.style.display = 'none';
+	popup.classList.remove('active');
+	overlay.classList.remove('active');
+	overlay.style.pointerEvents = 'none';
 
-    if (selectedNote) {
-        selectedNote.classList.remove('selected');
-        selectedNote = null;
-    }
+	if (selectedNote) {
+		selectedNote.classList.remove('selected');
+		selectedNote = null;
+	}
 
-    const dimmedElements = document.querySelectorAll('.note:not(.selected), #top, #newNoteButton, #aboutButton');
-    dimmedElements.forEach(element => {
-        element.classList.remove('dimmed');
-        element.classList.add('dimmedRestore');
-    });
+	const dimmedElements = document.querySelectorAll('.note:not(.selected), #top, #newNoteButton, #aboutButton');
+	dimmedElements.forEach(element => {
+		element.classList.remove('dimmed');
+		element.classList.add('dimmedRestore');
+	});
 }
 
 function deleteNote(index) {
-    let notes = localStorage.getItem('notes');
-    if (notes === null) {
-        return;
-    } else {
-        notes = JSON.parse(notes);
-    }
-    notes.splice(index, 1);
-    localStorage.setItem('notes', JSON.stringify(notes));
-    showNotes();
+	let notes = localStorage.getItem('notes');
+	if (notes === null) {
+		return;
+	} else {
+		notes = JSON.parse(notes);
+	}
+	notes.splice(index, 1);
+	localStorage.setItem('notes', JSON.stringify(notes));
+	showNotes();
 }
 
 function openEditor(event) {
-    const noteIndex = event.target.closest('.note').dataset.index;
-    notesContainer.classList.add('moveToLeft');
-    pageTitle.classList.add('moveToLeft');
-    appName.classList.remove('fadeIn');
-    appName.style.opacity = 1;
-    appName.classList.add('fadeOutAndMove');
-    pageTitle.classList.add('fadeOut');
-    notesContainer.classList.add('fadeOut');
-    event.target.closest('.note').classList.remove('dimmedRestore');
-    event.target.closest('.note').classList.add('titleAnimation');
+	const noteIndex = event.target.closest('.note').dataset.index;
+	notesContainer.classList.add('moveToLeft');
+	pageTitle.classList.add('moveToLeft');
+	appName.classList.remove('fadeIn');
+	appName.style.opacity = 1;
+	appName.classList.add('fadeOutAndMove');
+	pageTitle.classList.add('fadeOut');
+	notesContainer.classList.add('fadeOut');
+	event.target.closest('.note').classList.remove('dimmedRestore');
+	event.target.closest('.note').classList.add('titleAnimation');
 
-    window.setTimeout(() => {
-        window.location.href = `editor.html?index=${noteIndex}`, '_blank';
-    }, 500);
+	window.setTimeout(() => {
+		window.location.href = `editor.html?index=${noteIndex}`, '_blank';
+	}, 500);
 }
+
+// *** Código para o tema ***
+let theme = localStorage.getItem('theme') || 'light';
+
+function sendColorToNative(theme) {
+	if (window.hasOwnProperty('InterfaceAndroid')) {
+		InterfaceAndroid.changeColor(theme);
+	}
+}
+
+function applyTheme(theme) {
+	const img = document.getElementById('img');
+	let mainEls = document.querySelectorAll('#top, #notesContainer');
+	if (theme === 'dark') {
+		document.body.classList.add('darktheme');
+		mainEls.forEach(el => el.classList.add('darktheme'));
+		appbar.classList.add('dthemeabar');
+		newNoteButton.classList.add('dthemeabutton');
+		aboutButton.classList.add('dthemeabutton');
+		darkbtn.classList.add('dthemeabutton');
+		img.classList.add('dthemeabutton');
+	} else {
+		document.body.classList.remove('darktheme');
+		mainEls.forEach(el => el.classList.remove('darktheme'));
+		appbar.classList.remove('dthemeabar');
+		newNoteButton.classList.remove('dthemeabutton');
+		aboutButton.classList.remove('dthemeabutton');
+		darkbtn.classList.remove('dthemeabutton');
+		img.classList.remove('dthemeabutton');
+	}
+	sendColorToNative(theme);
+}
+
+// *** REMOVA ESTA LINHA: applyTheme(theme); ***
+
+function darkMode() {
+	theme = theme === 'dark' ? 'light': 'dark';
+	localStorage.setItem('theme', theme);
+	applyTheme(theme); // Chama applyTheme() SOMENTE quando o botão darkMode é clicado
+}
+
+// Restaura o tema *apenas se ele tiver sido salvo*
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+	applyTheme(savedTheme);
+}
+
+// *** Fim do código para o tema ***
